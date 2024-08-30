bedbf644fea7f05a507b1654cb05d21ec39d8613
BANDI ROHITH REDDY <139671817+rohith-bandi@users.noreply.github.com>
2024-08-30 14:24:42 +0530

    Delete flask-server directory

--- flask-server/backend/database.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://localhost:27017/")
-db = client['Login-Sigup']  # Use a single database for the whole application
-
-# Collections
-users_collection = db['logincollections']
-contact_collection = db['contact_messages']  # New collection for contact messages
