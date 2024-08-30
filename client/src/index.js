10b49f6c9419f59f6e7aaabdc00e5caf1cb8e612
BANDI ROHITH REDDY <139671817+rohith-bandi@users.noreply.github.com>
2024-08-30 14:24:32 +0530

    Delete client directory

--- client/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import Plot from 'react-plotly.js';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
